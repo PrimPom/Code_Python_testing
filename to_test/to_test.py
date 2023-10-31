5e6344e74607648ac131d5d366cdea7d7c1204ac
Primous POMALEGNI <primouspomalegni2@gmail.com>
2023-10-30 22:29:53 -0400

    Tests case with unittest for MathsOperations

---
+++ to_test/to_test.py
@@ -0,0 +1,40 @@
+class MathsOperations:
+    @classmethod
+    def addition(cls, a, b):
+        """
+        Fonction d'addition
+        :param a:
+        :param b:
+        :return: a+b
+        """
+        return a + b
+
+    @classmethod
+    def soustraction(cls, a, b):
+        """
+        Fonction de soustraction
+        :param a:
+        :param b:
+        :return: a-b
+        """
+        return a - b
+
+    @classmethod
+    def multiplication(cls, a, b):
+        """
+        Fonction de mutiplication
+        :param a:
+        :param b:
+        :return: a*b
+        """
+        return a*b
+
+    @classmethod
+    def division_entiere(cls, a, b):
+        """
+        Fonction de division entiere
+        :param a:
+        :param b:
+        :return: a//b
+        """
+        return a//b
